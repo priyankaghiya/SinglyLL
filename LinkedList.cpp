5cbfd8ce895f009b6ec148befcee321e3080bb37
Priyanka Ghiya <piyughiya12@gmail.com>
2020-12-05 22:44:47 -0800

    Deletion Completed With Finding Total Length of LL

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -7,6 +7,11 @@ void insert_at_begin(struct node *newnode,struct node *head,struct node *temp,in
 void insert_at_end(struct node *newnode,struct node *head,struct node *temp,int n);
 void insert_before_position(struct node *newnode,struct node *head,struct node *temp,int n);
 void insert_after_position(struct node *newnode,struct node *head,struct node *temp,int n);
+void delete_at_begin(struct node *newnode,struct node *head,struct node *temp,int n);
+void delete_at_end(struct node *newnode,struct node *head,struct node *temp,int n);
+void delete_at_position(struct node *newnode,struct node *head,struct node *temp,int n);
+void count_length(struct node *newnode,struct node *head,struct node *temp,int n);
+
 
 struct node
 {
@@ -125,6 +130,60 @@ void insert_after_position(struct node *newnode,struct node *head,struct node *t
 
 }
 
+void delete_at_begin(struct node *newnode,struct node *head,struct node *temp,int n)
+{
+    temp=head;
+    head=temp->next;
+    n--;
+    print_LL(head,temp,n);
+}
+
+void delete_at_end(struct node *newnode,struct node *head,struct node *temp,int n)
+{
+    temp=head;
+    struct node *prev;
+    while(temp->next!=NULL)
+    {
+        prev=temp;
+        temp=temp->next;
+    }
+    prev->next=NULL;
+    n--;
+    print_LL(head,temp,n);
+
+}
+
+void delete_at_position(struct node *newnode,struct node *head,struct node *temp,int n)
+{
+    int position;
+    cout<<"ENTER THE POSITION : ";
+    cin>>position;
+    temp=head;
+    struct node *prev;
+    for(int k=1;k<position;k++)
+    {
+        prev=temp;
+        temp=temp->next;
+    }
+    prev->next=temp->next;
+    n--;
+    print_LL(head,temp,n);
+
+}
+
+
+void count_length(struct node *newnode,struct node *head,struct node *temp,int n)
+    {
+        int count_len=0;
+        temp=head;
+        for(int l=1;l<=n;l++)
+        {
+            count_len++;
+            temp=temp->next;
+        }
+        cout<<"\nTHE TOTAL LENGTH OF THE LINKED LIST IS "<<count_len;
+    }
+
 
 int main()
 {
@@ -135,13 +194,19 @@ int main()
     head=create_linked_list(n,newnode,head,temp);
     print_LL(head,temp,n);
 
-    cout<<"\n 1 INSERT AT BEGINNING";
+
+       cout<<"\n 1 INSERT AT BEGINNING";
     cout<<"\n 2 INSERT AT END";
     cout<<"\n 3 INSERT BEFORE POSITION";
     cout<<"\n 4 INSERT AFTER POSITION";
+    cout<<"\n 5 DELETE AT BEGINNING";
+    cout<<"\n 6 DELETE AT END";
+    cout<<"\n 7 DELETE BEFORE POSITION";
+    cout<<"\n 8 PRESS FOR EXIT";
 
-     cout<<"\nENTER YOUR CHOICE : ";
+    cout<<"\nENTER YOUR CHOICE : ";
     cin>>choice;
+
 
     switch(choice)
         {
@@ -161,10 +226,27 @@ int main()
             insert_after_position(newnode,head,temp,n);
             break;
 
+        case 5:
+            delete_at_begin(newnode,head,temp,n);
+            break;
+
+        case 6:
+            delete_at_end(newnode,head,temp,n);
+            break;
+
+         case 7:
+            delete_at_position(newnode,head,temp,n);
+            break;
+
+
         default:
             cout<<"\nINVALID NUMBER";
 
         }
+
+
+    count_length(newnode,head,temp,n);
+
 
     return 0;
 }
